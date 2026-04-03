7071ace2d92778153fa482cd24e348805d87d337
AnaGabriellyrec <anagabrielly1322@gmail.com>
2026-04-03 02:56:41 -0300

    2

--- estoque.py
+++ estoque.py
@@ -1,5 +1,4 @@
 estoque = []
-produto = ""
 
 while True:
     print("\n1 - Adicionar produto")
@@ -8,24 +7,36 @@ while True:
     print("4 - Sair")
 
     opcao = input("Escolha uma opção: ")
-    if opcao == "1":
-        produto = input("Digite o nome do produto: ")
-        estoque.append(produto)
-        print("Produto adicionado com sucesso!")
 
-    elif opcao =="2":
-        produto = input("Digite o nome do produto a ser removido: ")
+    if opcao == "1":
+        produto = input("Digite o nome do produto: ").strip().lower()
+
+        if produto == "":
+            print("Não é permitido adicionar produto vazio.")
+        else:
+            estoque.append(produto)
+            print("Produto adicionado com sucesso!")
+
+    elif opcao == "2":
+        produto = input("Digite o nome do produto a ser removido: ").strip().lower()
+
         if produto in estoque:
             estoque.remove(produto)
             print("Produto removido com sucesso!")
         else:
             print("Produto não encontrado no estoque.")
-    elif opcao == "3":      
-        print("Estoque atual:")
-        for item in estoque:
-            print(f"- {item}")
+
+    elif opcao == "3":
+        if len(estoque) == 0:
+            print("Estoque vazio.")
+        else:
+            print("Estoque atual:")
+            for i, item in enumerate(estoque, start=1):
+                print(f"{i} - {item}")
+
     elif opcao == "4":
         print("Saindo...")
         break
+
     else:
         print("Opção inválida. Por favor, escolha uma opção válida.")
